e3b7b5e37e38a00d2e32b6001a2354ba00a3179d
King <kelkab46@gmail.com>
2023-01-25 10:07:10 +0300

    added homepage

---
+++ src/Components/homepage.js
@@ -0,0 +1,17 @@
+import react, { useState, useEffect } from "react";
+import Navbar from "./navbar";
+
+
+function Homepage() {
+
+
+    return (
+        <div>
+            <Navbar />
+            <Login />
+        </div>
+    )
+}
+
+export default Homepage
+
