0fd5033d877705c22082d798fa11c128f8a4adc8
Jesalim <jessyculture@gmail.com>
2023-01-25 11:07:01 +0300

    Navbar

---
+++ src/components/homepage.js
@@ -0,0 +1,18 @@
+// import react, { useState, useEffect } from "react";
+import Navbar from "./navbar";
+import Login from "./login";
+
+
+function Homepage() {
+
+
+    return (
+        <div>
+            <Navbar />
+            <Login />
+        </div>
+    )
+}
+
+export default Homepage
+
